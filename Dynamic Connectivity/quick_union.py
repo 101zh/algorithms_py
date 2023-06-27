d88752aa648b424da1eb08b521ff1fdd7e96f1e9
Hunter Zhuang <hunterzhuang0@gmail.com>
2023-06-27 14:35:15 -0700

    Coded QF in python sucessfully

---
+++ Dynamic Connectivity/quick_union.py
@@ -0,0 +1,16 @@
+class UF:
+    def __init__(self, n):
+        self.id=n*[None]
+        for i in range(n):
+            self.id[i]=i
+
+    def findRoot(self, x):
+        while self.id[x]!=x:
+            x=self.id[x]
+        return x
+
+    def connected(self, p, q):
+        return self.findRoot(p)==self.findRoot(q)
+
+    def union(self, p,q):
+        self.id[self.findRoot(p)]=self.findRoot(q)

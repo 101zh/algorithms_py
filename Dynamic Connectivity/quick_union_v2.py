c73cc4f718591fa7b1da0108d5b3bfdcddf37495
Hunter Zhuang <hunterzhuang0@gmail.com>
2023-06-28 16:18:07 -0700

    Made the quick_union with improvements
    - Does this by making trees shorter (results in wider tree)
    - By attaching the shorter tree the the root of the longer treee

---
+++ Dynamic Connectivity/quick_union_v2.py
@@ -0,0 +1,26 @@
+class QUI:
+    def __init__(self, n):
+        self.id=n*[None]
+        self.sz=n*[1]
+        for i in range(n):
+            self.id[i]=i
+
+    def root(self, x):
+        while self.id[x]!=x:
+            x=self.id[x]
+        return x
+
+    def connected(self, p, q):
+        return self.root(p)==self.root(q)
+
+    def union(self, p,q):
+        rootP=self.root(p)
+        rootQ=self.root(q)
+        if self.sz[rootP]<self.sz[rootQ]:
+            self.id[rootP]=self.id[rootQ]
+            self.sz[rootQ]+=self.sz[rootP]
+            self.sz[rootP]=1
+        else:
+            self.id[rootQ]=self.id[rootP]
+            self.sz[rootP]+=self.sz[rootQ]
+            self.sz[rootQ]=1
